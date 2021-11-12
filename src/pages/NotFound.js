ad9c6d1d87a415884301a63eab51c6d806357ac9
Alissandra <alissandraraysa@gmail.com>
2021-11-11 21:34:58 -0300

    iniciando o projeto

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>NOTFOUND</h1>
+      </div>
+    );
+  }
+}
